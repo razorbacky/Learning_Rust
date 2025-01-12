672a368fe238926df1dc48ddb0373342401fe69a
Yoon_Chanryul <razorbacky@gmail.com>
2025-01-12 21:59:13 +0900

    windows edited at home

--- learn/variable/src/main.rs
+++ learn/variable/src/main.rs
@@ -15,7 +15,7 @@ fn main() {
     // 상수 선언 const, 상수는 immutable이며 mutable로 바꿀 수 없다.
     // 또한 Rust에서 상수명은 모두 대문자로 작성하며 단어와 단어 사이에는 _(언더바)를 사용하여 분리한다.
     const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
-    /* 이 상수의 이름은 THREE_HOURS_IN_SCONDS이며, 값은 60(분당 초의 개수), 60(시간당 분의 개수),
+    /* 이 상수의 이름은 THREE_HOURS_IN_SECONDS 이며, 값은 60(분당 초의 개수), 60(시간당 분의 개수),
     3(이 프로그램에서 알아 둘 필요가 있는 시간의 숫자)를 모두 곱한 값이다. */
     // 참조 : https://doc.rust-lang.org/reference/const_eval.html //
     
--- learn/variable/target/debug/variable.d
+++ learn/variable/target/debug/variable.d
@@ -1,1 +1,1 @@
-/root/test/Learning_Rust/learn/variable/target/debug/variable: /root/test/Learning_Rust/learn/variable/src/main.rs
+C:\Users\razor\OneDrive\문서\dev\Rust\Learning_Rust\learn\variable\target\debug\variable.exe: C:\Users\razor\OneDrive\문서\dev\Rust\Learning_Rust\learn\variable\src\main.rs
