a557d8178bc908fd6863b3ff4489db93ad14d885
Yoon_Chanryul <razorbacky@gmail.com>
2025-01-14 23:56:57 +0900

    Function 3

--- learn/functional_3/target/debug/functional_3.d
+++ learn/functional_3/target/debug/functional_3.d
@@ -1,1 +1,1 @@
-/root/test/Learning_Rust/learn/functional_3/target/debug/functional_3: /root/test/Learning_Rust/learn/functional_3/src/main.rs
+C:\Users\razor\OneDrive\문서\dev\Rust\Learning_Rust\learn\functional_3\target\debug\functional_3.exe: C:\Users\razor\OneDrive\문서\dev\Rust\Learning_Rust\learn\functional_3\src\main.rs
